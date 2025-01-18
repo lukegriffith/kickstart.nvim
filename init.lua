209fe204603337861cfc47ec583092da206341b1
Luke Griffith <lukemgriffith@gmail.com>
2025-01-18 15:22:57 +0000

    Updating nvim config to be transparent

--- init.lua
+++ init.lua
@@ -869,6 +869,13 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
